d116ffa6ccd68c8bddcf33c68e9e5f650f9e2698
yeonsayho <kis7922@naver.com>
2024-09-09 00:29:02 +0900

    first commit

---
+++ src/main/java/justjemi/jemi/JemiApplication.java
@@ -0,0 +1,13 @@
+package justjemi.jemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JemiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JemiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/justjemi/jemi/yeonho/GitTest.java
@@ -0,0 +1,8 @@
+package justjemi.jemi.yeonho;
+
+public class GitTest {
+    public static void main(String[] args) {
+        int a=1;
+        System.out.println(a);
+    }
+}
